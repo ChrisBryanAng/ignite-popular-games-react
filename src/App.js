1e5d4126f40240e0fcee2abcda9768401a59dd31
ChrisBryanAng <ChrisBryanAng@gmail.com>
2021-01-14 16:41:36 -0800

    Added Global Styles and Added and displayed popular and new games

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 // components and pages
 import Home from "./pages/Home";
+// Global Styles
+import GlobalStyle from "./components/GlobalStyles";
 
 function App() {
 	return (
 		<div className="App">
+			<GlobalStyle />
 			<Home />
 		</div>
 	);
